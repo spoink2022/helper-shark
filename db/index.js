958cd21f6fc4ab9a84d342f4ba5ef49b55e64373
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2022-01-06 09:09:22 -0500

    update for new db

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const config = require('./config.js');
 
 module.exports.getPurchases = async function(userid) {
-    let query = 'SELECT * FROM purchases WHERE userid=$1 LIMIT 1';
+    let query = 'SELECT supporter, big_supporter WHERE userid=$1 LIMIT 1';
     let res = await config.pquery(query, [userid]);
     return res[0];
 }
