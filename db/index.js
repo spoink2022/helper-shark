fd9694b4f5afb74d4afc6006efabcab64c64d87f
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2022-05-14 17:15:12 -0400

    bonus giveaway

--- db/index.js
+++ db/index.js
@@ -5,3 +5,15 @@ module.exports.getPurchases = async function(userid) {
     let res = await config.pquery(query, [userid]);
     return res[0];
 }
+
+module.exports.updateUserColumns = async function(userid, obj) {
+    let queryMiddle = Object.keys(obj).map(key => `${key}=${key}+${obj[key]}`).join(', ');
+    let query = `UPDATE users SET ${queryMiddle} WHERE userid=$1`;
+    return await config.pquery(query, [userid]);
+}
+
+module.exports.updateBaitColumns = async function(userid, obj) {
+    let queryMiddle = Object.keys(obj).map(key => `${key}=${key}+${obj[key]}`).join(', ');
+    let query = `UPDATE bait SET ${queryMiddle} WHERE userid=$1`;
+    return await config.pquery(query, [userid]);
+}
